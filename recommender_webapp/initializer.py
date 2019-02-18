707408f59163f3258202daee0eae3914c4168f0f
gperniola <g.perniola22@gmail.com>
2019-02-18 13:15:34 +0100

    refactoring

--- recommender_webapp/initializer.py
+++ recommender_webapp/initializer.py
@@ -1,3 +1,4 @@
-from recommender_webapp.common.data_loader import DataLoader
+#from recommender_webapp.common.data_loader import DataLoader
+from api.common.data_loader import DataLoader
 
 data_loader = DataLoader()
